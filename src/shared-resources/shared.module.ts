52e99d9a025d42ae2bac5db9ccd202788df0f1d6
AlexDeKatz <kplihkith@gmail.com>
2020-07-11 19:21:04 +0530

    Added custom theming

---
+++ src/shared-resources/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatButtonModule
+  ]
+})
+export class SharedModule { }
